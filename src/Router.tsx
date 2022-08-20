329d7b30e508c560e55191697a68f6c989fc5460
Pedro Lucas <88797361+pelucs@users.noreply.github.com>
2022-08-20 13:49:44 -0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Category from './pages/Category';
 
 import Home from './pages/Home';
-import Product from './pages/Product';
 import Purchase from './pages/FinalizePurchase';
 import PageDevelopment from './pages/PageDevelopment';
 
@@ -10,7 +9,6 @@ export default () => {
   return(
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/product/:departament/:slug" element={<Product/>}/>
       <Route path="/:category" element={<Category/>}/>
       <Route path="/purchase" element={<Purchase/>}/>
       <Route path="/development" element={<PageDevelopment/>}/> 
